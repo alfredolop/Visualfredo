b57cfca523ddb23f4d29c51d1585383ffd4926a5
alfredolop <alfredolop710@gmail.com>
2020-06-02 19:11:44 +0200

    Creacion del proyecto

---
+++ Visualfredo/Visualfredo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Visualfredo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
